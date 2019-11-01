54b6bf08326263704f6cc4e9e23c9b9e912e2a0f
Sahar <hello@sahar.pk>
2019-11-01 22:23:50 +1000

    Update BlynkServerAutoInstallerWin.ps1

--- BlynkServerAutoInstallerWin.ps1
+++ BlynkServerAutoInstallerWin.ps1
@@ -10,47 +10,43 @@ Begin {
 	#Write-Warning "You are not running this script as an administrator. Run it again as administrator." ;
 	break
 	}
+
 $BlynkServerVer="0.41.11"
 $LibraryVer="0.6.1"
-$start_time = Get-Date
-Write-Host "`r`n"
+
 Write-Host "This script will download and install Blynk Local Server on your PC/Laptop." -ForegroundColor Red -BackgroundColor Yellow
 Write-Host "`r`n"
 
-#================================================================================================
 # First Download and install Blynk Library
-# Fetch Blynk Library from GitHub
+Write-Host "Fetch Blynk Library from GitHub" -ForegroundColor Red -BackgroundColor Yellow
 $url = "https://github.com/blynkkk/blynk-library/releases/download/v" + $LibraryVer + "/Blynk_Release_v" + $LibraryVer + ".zip"
 $output = "$PSScriptRoot\Blynk_Release_v$LibraryVer.zip"
 Invoke-WebRequest -Uri $url -OutFile $output
+
 # Unzip Library in Arduino Folder
 $LibraryPath = "C:\Users\$env:UserName\Documents\Arduino"
-expand-archive "Blynk_Release_v$LibraryVer.zip" -destinationpath $LibraryPath
+expand-archive "Blynk_Release_v$LibraryVer.zip" -destinationpath $LibraryPath -Force
+
 # Remove downloaded Library
-rmdir Blynk_Release_v$LibraryVer.zip
+Remove-Item Blynk_Release_v$LibraryVer.zip
 
-#================================================================================================
-# Remove folder and files if there were already downloaded.
-#Remove-Item BlynkLocalServer -Force -Recurse -ErrorAction Ignore
-rmdir C:\BlynkLocalServer
+# Define Server location
+$ServerPath = "C:\Users\$env:UserName\Documents\BlynkLocalServer"
 
-#================================================================================================
-# Create folder
-#New-Item BlynkLocalServer -ItemType "directory"
-mkdir C:\BlynkLocalServer
+# Remove blynk server folder if already present.
+Remove-Item $ServerPath -Force -Recurse -ErrorAction Ignore
 
-#================================================================================================
-# Go to created folde
-cd C:\BlynkLocalServer
-[console]::beep(2000,500)
+# Create folder for putting blynk server .jar file.
+New-Item -ItemType "directory" -Path $ServerPath
 
-#================================================================================================
-# Fetch blynk local server from GitHub
+# Fetch Blynk Local server from GitHub
 $url = "https://github.com/blynkkk/blynk-server/releases/download/v" + $BlynkServerVer + "/server-" + $BlynkServerVer + ".jar"
 $output = "$PSScriptRoot\server-$BlynkServerVer.jar"
 Invoke-WebRequest -Uri $url -OutFile $output
 
-#================================================================================================
+# Move server file to server folder
+Move-Item "server-$BlynkServerVer.jar" -Destination $ServerPath
+
 # Allow Less Secure Apps Access:ON
 Start-Process "https://myaccount.google.com/lesssecureapps"
 # Wait for process to complete
@@ -61,9 +57,7 @@ $MessageBody = "Check the webpage opened for you. Please Allow Less Secure Apps:
 $MessageTitle = "Grant Gmail Access"
 $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
 
-#================================================================================================
 # Enable mail on local server
-Write-Host "`r`n"
 $gMail = Read-Host -Prompt "What is your gmail address? :"
 Write-Host "`r`n"
 $gMailPass = Read-Host -Prompt "What is your gmail password? :"
@@ -75,45 +69,21 @@ Add-Content mail.properties mail.smtp.port=587
 Add-Content mail.properties mail.smtp.username=$gMail
 Add-Content mail.properties mail.smtp.password=$gMailPass
 
-#================================================================================================
+# Move mail properties file to server folder
+Move-Item mail.properties -Destination $ServerPath -Force
+
 # Open ports for blynk
 netsh advfirewall firewall add rule name="OpenPort 80" dir=in action=allow protocol=TCP localport=80
 netsh advfirewall firewall add rule name="OpenPort 443" dir=in action=allow protocol=TCP localport=443
 netsh advfirewall firewall add rule name="OpenPort 8080" dir=in action=allow protocol=TCP localport=8080
 netsh advfirewall firewall add rule name="OpenPort 9443" dir=in action=allow protocol=TCP localport=9443
 
-#================================================================================================
-# Run Blynk Server
-java -jar server-$BlynkServerVer.jar -dataFolder BlynkLocalServer
-
-
-#================================================================================================
 # Create Auto Start for Blynk Server
 New-Item start-blynk.bat
-$BlynkCMD = "java -jar server-"+ $BlynkServerVer+".jar -dataFolder BlynkLocalServer"
+$BlynkCMD = "java -jar $ServerPath\server-"+ $BlynkServerVer+".jar -dataFolder $ServerPath"
 Set-Content start-blynk.bat $BlynkCMD
 Move-Item start-blynk.bat -Destination "C:\Users\${env:UserName}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-[console]::beep(2000,500)
 
-#================================================================================================
-# New line for spacing
-Clear-Host
-Write-Host "`r`n"
-Write-Host "`nYou can access blynk server admin page at https://127.0.0.1:9443/admin`n"
-Write-Host "`r`n"
-Write-Host "`r`n"
-Write-Host "`nPlease restart your laptop/PC `n"
-
-#================================================================================================
-Write-Host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" -ForegroundColor Red -BackgroundColor Yellow
-[console]::beep(2000,500)
-(New-Object -com SAPI.SpVoice).speak("Blynk local server is installed successfully.")
-# Display Ok Box
-Add-Type -AssemblyName PresentationCore,PresentationFramework
-$ButtonType = [System.Windows.MessageBoxButton]::Ok
-$MessageIcon = [System.Windows.MessageBoxImage]::Information
-$MessageBody = "Your laptop/PC will be restarted now."
-$MessageTitle = "Server Installed"
-$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
-restart-computer
+# Run Blynk Server
+java -jar "$ServerPath\server-$BlynkServerVer.jar" -dataFolder $ServerPath
 }
